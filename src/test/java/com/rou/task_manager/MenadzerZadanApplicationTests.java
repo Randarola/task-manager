5ffabbe5801e093b20f4bacd3e4ad455e173a3a7
Randarola <140419343+Randarola@users.noreply.github.com>
2026-01-18 15:24:03 +0100

    First commit

---
+++ src/test/java/com/rou/task_manager/MenadzerZadanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rou.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenadzerZadanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
