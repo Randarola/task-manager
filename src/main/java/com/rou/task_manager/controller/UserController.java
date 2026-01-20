050082adc00762b9f55c5bc9ab581dd891f60a35
Randarola <140419343+Randarola@users.noreply.github.com>
2026-01-20 17:52:10 +0100

    add user model

--- src/main/java/com/rou/task_manager/controller/UserController.java
+++ src/main/java/com/rou/task_manager/controller/UserController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/users")
     public String getUser(){
-        return "Lista użytkowników";
+        return "Lista";
     }
 }
