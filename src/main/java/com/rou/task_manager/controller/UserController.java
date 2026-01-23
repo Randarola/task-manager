f62487c1f0bbf346eb6bfb2bb7f2c40a1189bfde
Randarola <140419343+Randarola@users.noreply.github.com>
2026-01-23 16:25:55 +0100

    add addUser

--- src/main/java/com/rou/task_manager/controller/UserController.java
+++ src/main/java/com/rou/task_manager/controller/UserController.java
@@ -3,6 +3,8 @@ package com.rou.task_manager.controller;
 import com.rou.task_manager.model.User;
 import com.rou.task_manager.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,12 +12,18 @@ import java.util.List;
 
 @RestController
 public class UserController {
+
     private final UserRepository userRepository;
     public UserController (UserRepository userRepository){
         this.userRepository = userRepository;
     }
+
     @GetMapping("/users")
     public List <User> getUsers(){
         return userRepository.findAll();
+    }
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        return userRepository.save(user);
     }
 }
--- src/main/java/com/rou/task_manager/model/User.java
+++ src/main/java/com/rou/task_manager/model/User.java
@@ -1,11 +1,9 @@
 package com.rou.task_manager.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     private String username;
     private String password;
