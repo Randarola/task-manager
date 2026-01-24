d8627d719f75c2a6ec5fc40694b92bd01644c1a6
Randarola <140419343+Randarola@users.noreply.github.com>
2026-01-24 17:56:47 +0100

    add Role

--- src/main/java/com/rou/task_manager/model/User.java
+++ src/main/java/com/rou/task_manager/model/User.java
@@ -1,16 +1,25 @@
 package com.rou.task_manager.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     private String username;
     private String password;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public User() {
     }
@@ -28,6 +37,11 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     public String getPassword() {
@@ -53,4 +67,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+}
+
+enum Role{
+    USER, ADMIN
 }
