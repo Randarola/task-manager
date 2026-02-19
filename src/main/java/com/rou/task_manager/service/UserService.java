2ff55b81b57267cb87b503e848bda9d23f587994
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-19 10:46:51 +0100

    add password encoder

--- src/main/java/com/rou/task_manager/service/UserService.java
+++ src/main/java/com/rou/task_manager/service/UserService.java
@@ -5,6 +5,8 @@ import com.rou.task_manager.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +18,18 @@ import java.util.Optional;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository){
+
         this.userRepository = userRepository;
+        this.passwordEncoder =new BCryptPasswordEncoder();
     }
     public List<User> getUsers(){
         return userRepository.findAll();
     }
     public User addUser(@RequestBody User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save((user));
     }
 
