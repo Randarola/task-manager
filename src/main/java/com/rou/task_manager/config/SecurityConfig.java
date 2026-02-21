1ed2aa5fd446736ebf995c4e4c92ef9e9853317c
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-21 23:09:03 +0100

    rename UsingConfig into SecurityConfig

---
+++ src/main/java/com/rou/task_manager/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.rou.task_manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                .requestMatchers("/users/**").authenticated()
+                .anyRequest().permitAll()
+        ).formLogin(form ->form.permitAll());
+        return http.build();
+    }
+}
