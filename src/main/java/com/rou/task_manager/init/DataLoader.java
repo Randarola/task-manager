cd622817b1323b789ce3da7e5367a5b4b4263c71
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-23 12:55:35 +0100

    init admin

--- src/main/java/com/rou/task_manager/init/DataLoader.java
+++ src/main/java/com/rou/task_manager/init/DataLoader.java
@@ -2,6 +2,7 @@ package com.rou.task_manager.init;
 
 import com.rou.task_manager.model.Role;
 import com.rou.task_manager.model.User;
+import com.rou.task_manager.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
     private final PasswordEncoder passwordEncoder;
-    public DataLoader(PasswordEncoder passwordEncoder){
+    private final UserRepository userRepository;
+
+    public DataLoader(PasswordEncoder passwordEncoder, UserRepository userRepository){
         this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
     @Override
     public void run(String... args) throws Exception {
         User admin = new User("admin","admin@gmail.com",passwordEncoder.encode("admin123"), Role.ADMIN);
+        userRepository.save(admin);
     }
 }
