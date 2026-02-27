3401da31e2c52476cb831ce64d0dcbf5b76ba0dd
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-27 21:33:52 +0100

    add findByUser method in TaskRepository

--- src/main/java/com/rou/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/rou/taskmanager/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.rou.taskmanager.repository;
 
 import com.rou.taskmanager.model.Task;
+import com.rou.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository <Task, Integer> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository <Task, Integer> {
+    List<Task> findByUser(User user);
 }
