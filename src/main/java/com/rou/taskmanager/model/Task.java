ed43e6976d87152bfedc36327686fd650947cba5
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-28 08:35:58 +0100

    generate setters and getters

--- src/main/java/com/rou/taskmanager/model/Task.java
+++ src/main/java/com/rou/taskmanager/model/Task.java
@@ -10,6 +10,30 @@ public class Task {
     private String title;
     private String description;
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
