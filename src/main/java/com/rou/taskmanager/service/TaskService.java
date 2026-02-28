fd7e2d103a4f8df5b2d5490d093df6fc9b3dc4a2
Randarola <140419343+Randarola@users.noreply.github.com>
2026-02-28 08:35:39 +0100

    add task methods for user

--- src/main/java/com/rou/taskmanager/service/TaskService.java
+++ src/main/java/com/rou/taskmanager/service/TaskService.java
@@ -1,7 +1,11 @@
 package com.rou.taskmanager.service;
 
+import com.rou.taskmanager.model.Task;
+import com.rou.taskmanager.model.User;
 import com.rou.taskmanager.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService{
@@ -11,4 +15,14 @@ public class TaskService{
     public TaskService(TaskRepository taskRepository){
         this.taskRepository = taskRepository;
     }
+
+    public List<Task> getTasksForUser(User user){
+        return taskRepository.findByUser(user);
+    }
+
+    public Task addTask(Task task, User user){
+        task.setUser(user);
+        return taskRepository.save(task);
+    }
+    
 }
