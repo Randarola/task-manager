3eeab7c9b9fa467d3756e8da25217fb655f60973
Randarola <140419343+Randarola@users.noreply.github.com>
2026-03-03 18:27:24 +0100

    add taskRepository  to DataLoader

--- src/main/java/com/rou/taskmanager/init/DataLoader.java
+++ src/main/java/com/rou/taskmanager/init/DataLoader.java
@@ -1,7 +1,9 @@
 package com.rou.taskmanager.init;
 
 import com.rou.taskmanager.model.Role;
+import com.rou.taskmanager.model.Task;
 import com.rou.taskmanager.model.User;
+import com.rou.taskmanager.repository.TaskRepository;
 import com.rou.taskmanager.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,10 +15,12 @@ import java.util.List;
 public class DataLoader implements CommandLineRunner {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final TaskRepository taskRepository;
 
-    public DataLoader(PasswordEncoder passwordEncoder, UserRepository userRepository){
+    public DataLoader(PasswordEncoder passwordEncoder, UserRepository userRepository, TaskRepository taskRepository){
         this.passwordEncoder = passwordEncoder;
         this.userRepository = userRepository;
+        this.taskRepository = taskRepository;
     }
     @Override
     public void run(String... args) throws Exception {
@@ -24,6 +28,7 @@ public class DataLoader implements CommandLineRunner {
             User admin = new User("admin", "admin@gmail.com", passwordEncoder.encode("admin123"), Role.ADMIN);
             User user = new User("user", "user@gmail.com", passwordEncoder.encode("user123"), Role.USER);
             userRepository.saveAll(List.of(admin, user));
+
         }
     }
 }
